7914ac496762b732e3cd609e3cfe51be2618863a
architghetiya007 <ghetiyarchit007@gmail.com>
2022-04-30 10:20:02 +0530

    change credetial and new build

--- public/angular/main.js
+++ public/angular/main.js
@@ -1088,7 +1088,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
 const environment = {
     production: false,
-    baseUrl: '/apisd'
+    baseUrl: '/api'
 };
 
 
